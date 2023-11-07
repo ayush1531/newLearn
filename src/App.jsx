3040dd79b0a315a1a47dc9160e4e8106a2f41453
ayush bothra <ayush.bothra@synoriq.in>
2023-11-07 14:55:04 +0530

    routes and file created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
 export default function App() {
-  return (
-    <h1 className="text-red-500">App</h1>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/About" element={<About/>}/>
+    <Route path="/Profile" element = {<Profile/>}/>
+    <Route path="/Sign-out" element = {<Signup/>}/>
+    <Route path="/Sign-in" element = {<Signin/>}/>
+  </Routes>
+  </BrowserRouter>
 }
